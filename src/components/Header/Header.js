6f01b8d96dea9a83bbb11fea25b3fd9451799ab6
Amir Mohammad Fallah <amirmfallah@yahoo.com>
2022-02-27 00:48:16 +0400

    [add] connect to wallet

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,19 +2,5 @@ import React from "react";
 import "./Header.css";
 
 export default function About() {
-  return (
-    <div className="d-flex flex-row align-items-center container my-3">
-      <span className="header-title">0xLions V1</span>
-      <div className="flex-1 flex-grow-1"></div>
-      <div className="connect-btn mr-2">
-        <img
-          src="/assets/metamask.png"
-          alt="metamask"
-          height={24}
-          className="metamask"
-        ></img>
-        <span>Connect Wallet</span>
-      </div>
-    </div>
-  );
+  return <></>;
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -7,66 +7,254 @@ import { faMinus, faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import "./Main.css";
+import Web3 from "web3";
+import Web3Modal from "web3modal";
+import WalletConnectProvider from "@walletconnect/web3-provider";
+import * as Swal from "sweetalert2";
 
-export default function Main() {
-  return (
-    <div className="main-section container pt-3 pb-3">
-      <div className="row">
-        <div className="col-12 col-md-6 col-lg-6 d-flex flex-column align-items-center">
-          <h3 className="main-title">
-            0xLions V1 is a Collection of 6000 Unique Regenerated Lions living
-            on the Ethereum blockchain.
-          </h3>
-          <p>
-            0xLions V1 is not affiliated with Lazy Lions or any other NFT
-            projects.
-          </p>
-          <p>
-            In a parallel universe, a DNA splicing disease has rapidly spread
-            across the 0xLions private island. More than half of the population
-            has been infected by this DNA splicing disease. Due to the extreme
-            nature of this sickness, the 0xLions have been forced into
-            quarantine. Those infected seem to be experiencing extreme
-            transformations drastically affecting their appearance. Doctors and
-            scientists across the island are intensely researching and heavily
-            experimenting this and have yet to derive a solution…. To be
-            continued.
-          </p>
-          <div className="d-flex flex-row align-items-center mt-3 mb-3">
-            <div className="round-btn">
-              <FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon>
-            </div>
-            <div className="round-btn">
-              <FontAwesomeIcon icon={faDiscord}></FontAwesomeIcon>
-            </div>
-            <div className="round-btn">
-              <img src="/assets/opensea.svg" alt="opensea" height="16px"></img>
-            </div>
-            <div className="round-btn no-margin">
-              <FontAwesomeIcon icon={faEthereum}></FontAwesomeIcon>
-            </div>
+const providerOptions = {
+  /* See Provider Options Section */
+  walletconnect: {
+    package: WalletConnectProvider,
+    options: {
+      infuraId: "a6a041ec814f43d8aef380b4cd8a20df", // required
+      network: "mainnet",
+    },
+  },
+};
+
+const contractABI = require("../../XDoodlesNFTV3.json");
+const NFT_ABI = contractABI.abi;
+const NFT_CONTRACT_ADDRESS = "";
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 1,
+      wallet: undefined,
+      minting: false,
+      web3: undefined,
+      supply: 0,
+    };
+
+    this.inc = this.inc.bind(this);
+    this.dec = this.dec.bind(this);
+    this.connectWallet = this.connectWallet.bind(this);
+    this.mint = this.mint.bind(this);
+    this.loadCount = this.loadCount.bind(this);
+    this.loadInstance = this.loadInstance.bind(this);
+
+    //this.loadInstance();
+  }
+  web3Instance;
+  contractInstance;
+
+  async loadInstance() {
+    this.web3Instance = new Web3(
+      "https://rinkeby.infura.io/v3/a6a041ec814f43d8aef380b4cd8a20df"
+    );
+    this.contractInstance = await new this.web3Instance.eth.Contract(
+      NFT_ABI,
+      NFT_CONTRACT_ADDRESS
+    );
+    setInterval(this.timer.bind(this), 5000);
+  }
+
+  web3Modal = new Web3Modal({
+    network: "mainnet", // optional
+    cacheProvider: false, // optional
+    providerOptions, // required
+  });
+
+  inc() {
+    if (this.state.minting) {
+      return;
+    }
+    if (this.state.count >= 10) {
+      return;
+    }
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  dec() {
+    if (this.state.minting) {
+      return;
+    }
+    if (this.state.count > 1) {
+      this.setState({ count: this.state.count - 1 });
+    }
+  }
+
+  async timer() {
+    if (!this.state.minting) {
+      await this.loadCount();
+    }
+  }
+
+  async loadCount(web) {
+    try {
+      this.contractInstance.methods
+        .totalSupply()
+        .call()
+        .then((count) => {
+          this.setState({ supply: count });
+        });
+    } catch (e) {
+      await this.loadInstance();
+    }
+  }
+
+  async connectWallet() {
+    if (this.state.wallet) {
+      this.setState({ wallet: undefined });
+      return;
+    }
+    const provider = await this.web3Modal.connect();
+    this.web3Instance = this.setState({ web3: new Web3(provider) });
+    this.setState({ wallet: (await this.state.web3.eth.getAccounts())[0] });
+    console.log(this.state.wallet);
+  }
+
+  async mint() {
+    if (this.state.minting) {
+      return;
+    }
+    const contract = await new this.state.web3.eth.Contract(
+      NFT_ABI,
+      NFT_CONTRACT_ADDRESS
+    );
+    this.setState({ minting: true });
+
+    const nonce = await this.state.web3.eth.getTransactionCount(
+      this.state.wallet,
+      "latest"
+    ); //get latest nonce
+
+    let free = 1;
+    console.log(parseInt(this.state.supply), parseInt(this.state.count));
+    if (parseInt(this.state.supply) + parseInt(this.state.count) <= 1100) {
+      free = 0;
+    }
+    console.log(free);
+    //the transaction
+    const tx = {
+      from: this.state.wallet,
+      to: NFT_CONTRACT_ADDRESS,
+      nonce: nonce,
+      maxPriorityFeePerGas: 2999999987,
+      value: this.state.web3.utils.toWei(
+        (0.01 * this.state.count * free).toString(),
+        "ether"
+      ),
+    };
+
+    contract.methods
+      .mintItem(this.state.web3.utils.toBN(this.state.count))
+      .send(tx)
+      .then(async () => {
+        await Swal.fire({
+          title: "Congratulations!",
+          text: "You just minted a 0xDoodlesNFT V3. Welcome to the family.",
+          icon: "success",
+          heightAuto: false,
+        });
+        this.setState({ minting: false });
+      })
+      .catch(async (error) => {
+        await Swal.fire({
+          title: "Oops!",
+          text: "Something went wrong. Please try again later.",
+          icon: "error",
+          heightAuto: false,
+        });
+        this.setState({ minting: false });
+      });
+  }
+
+  render() {
+    return (
+      <div className="main-section container pt-3 pb-3">
+        <div className="d-flex flex-row align-items-center mb-3">
+          <span className="header-title">0xLions V1</span>
+          <div className="flex-1 flex-grow-1"></div>
+          <div className="connect-btn mr-2" onClick={this.connectWallet}>
+            <img
+              src="/assets/metamask.png"
+              alt="metamask"
+              height={24}
+              className="metamask"
+            ></img>
+            <span>
+              {this.state.wallet ? "Disconnect Wallet" : "Connect Wallet"}
+            </span>
           </div>
         </div>
-        <div className="col-12 col-md-6 col-lg-6 d-flex flex-column justify-content-center align-items-center">
-          <img src="/assets/1.jpeg" alt="sneakpeak" className="w-100"></img>
-          <span className="text-center mt-3">
-            One 0xLions V1 NFT cost 0.02 ETH
-          </span>
-          <h2 className="text-center mt-3">0/6000</h2>
-          <div className="d-flex flex-row justify-content-center align-items-center mt-2">
-            <div className="d-flex flex-row justify-content-center align-items-center count-container">
+        <div className="row">
+          <div className="col-12 col-md-6 col-lg-6 d-flex flex-column align-items-center">
+            <h3 className="main-title">
+              0xLions V1 is a Collection of 6000 Unique Regenerated Lions living
+              on the Ethereum blockchain.
+            </h3>
+            <p>
+              0xLions V1 is not affiliated with Lazy Lions or any other NFT
+              projects.
+            </p>
+            <p>
+              In a parallel universe, a DNA splicing disease has rapidly spread
+              across the 0xLions private island. More than half of the
+              population has been infected by this DNA splicing disease. Due to
+              the extreme nature of this sickness, the 0xLions have been forced
+              into quarantine. Those infected seem to be experiencing extreme
+              transformations drastically affecting their appearance. Doctors
+              and scientists across the island are intensely researching and
+              heavily experimenting this and have yet to derive a solution…. To
+              be continued.
+            </p>
+            <div className="d-flex flex-row align-items-center mt-3 mb-3">
               <div className="round-btn">
-                <FontAwesomeIcon icon={faMinus}></FontAwesomeIcon>
+                <FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon>
               </div>
-              <span className="count">1</span>
-              <div className="round-btn m-0">
-                <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
+              <div className="round-btn">
+                <FontAwesomeIcon icon={faDiscord}></FontAwesomeIcon>
+              </div>
+              <div className="round-btn">
+                <img
+                  src="/assets/opensea.svg"
+                  alt="opensea"
+                  height="16px"
+                ></img>
+              </div>
+              <div className="round-btn no-margin">
+                <FontAwesomeIcon icon={faEthereum}></FontAwesomeIcon>
               </div>
             </div>
-            <button className="btn2">Mint</button>
+          </div>
+          <div className="col-12 col-md-6 col-lg-6 d-flex flex-column justify-content-center align-items-center">
+            <img src="/assets/1.jpeg" alt="sneakpeak" className="w-100"></img>
+            <span className="text-center mt-3">
+              One 0xLions V1 NFT cost 0.02 ETH
+            </span>
+            <h2 className="text-center mt-3">{this.state.supply}/6000</h2>
+            <div className="d-flex flex-row justify-content-center align-items-center mt-2">
+              <div className="d-flex flex-row justify-content-center align-items-center count-container">
+                <div className="round-btn" onClick={this.dec}>
+                  <FontAwesomeIcon icon={faMinus}></FontAwesomeIcon>
+                </div>
+                <span className="count">{this.state.count}</span>
+                <div className="round-btn m-0" onClick={this.inc}>
+                  <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
+                </div>
+              </div>
+              <button className="btn2" disabled={!this.state.wallet}>
+                Mint
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+export default Main;
